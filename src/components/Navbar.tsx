26180cece1158bea75fad9fd0b16a76eecec280c
spalivel <spalivel@usc.edu>
2024-04-30 19:06:18 -0700

    Add kinde authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import { buttonVariants } from './ui/button';
-
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
 const Navbar = () => {
 	return (
 		<nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b borer-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -23,6 +24,13 @@ const Navbar = () => {
 							>
 								Pricing
 							</Link>
+							<LoginLink className={buttonVariants({
+                                variant:'ghost',
+                                size:'sm'
+                            })}>Sign In</LoginLink>
+                            <RegisterLink className={buttonVariants({
+                                size:'sm'
+                            })}>Get started <ArrowRight className='ml:1.5 h:5 w:5'/></RegisterLink>
 						</>
 					</div>
 				</div>
